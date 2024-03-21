7cb861de5799bfb4c5c9a06c60a1b3bb5e2c87ad
incubator4 <aries0robin@gmail.com>
2024-03-21 23:30:56 +0800

    fix(finalize): graceful remove finalizer

--- internal/controller/factory/finalize.go
+++ internal/controller/factory/finalize.go
@@ -3,7 +3,6 @@ package factory
 import (
 	"context"
 	nodev1alpha1 "github.com/rss3-network/node-operator/api/v1alpha1"
-	"github.com/samber/lo"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
@@ -11,12 +10,6 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 )
-
-func finalizerWithoutNode(finalizers []string) []string {
-	return lo.Filter(finalizers, func(s string, index int) bool {
-		return s != nodev1alpha1.NodeFinalizer
-	})
-}
 
 func AddFinalizer(ctx context.Context, rc client.Client, instance client.Object) error {
 	if !controllerutil.ContainsFinalizer(instance, nodev1alpha1.NodeFinalizer) {
@@ -28,7 +21,7 @@ func AddFinalizer(ctx context.Context, rc client.Client, instance client.Object)
 
 func RemoveFinalizer(ctx context.Context, rc client.Client, instance client.Object) error {
 	if controllerutil.ContainsFinalizer(instance, nodev1alpha1.NodeFinalizer) {
-		instance.SetFinalizers(finalizerWithoutNode(instance.GetFinalizers()))
+		controllerutil.RemoveFinalizer(instance, nodev1alpha1.NodeFinalizer)
 		return rc.Update(ctx, instance)
 	}
 	return nil
@@ -45,16 +38,16 @@ func removeFinalizeObjByName(ctx context.Context, rclient client.Client, obj cli
 	if !controllerutil.ContainsFinalizer(obj, nodev1alpha1.NodeFinalizer) {
 		return nil
 	}
-	obj.SetFinalizers(finalizerWithoutNode(obj.GetFinalizers()))
+	controllerutil.RemoveFinalizer(obj, nodev1alpha1.NodeFinalizer)
 	return rclient.Update(ctx, obj)
 }
 
 func OnHubDelete(ctx context.Context, rc client.Client, instance *nodev1alpha1.Hub) (err error) {
-	if err = removeFinalizeObjByName(ctx, rc, &appsv1.Deployment{}, instance.Name, instance.Namespace); err != nil {
+	if err = removeFinalizeObjByName(ctx, rc, &corev1.Service{}, instance.Name, instance.Namespace); err != nil {
 		return err
 	}
 
-	if err = removeFinalizeObjByName(ctx, rc, &corev1.Service{}, instance.Name, instance.Namespace); err != nil {
+	if err = removeFinalizeObjByName(ctx, rc, &appsv1.Deployment{}, instance.Name, instance.Namespace); err != nil {
 		return err
 	}
 
