3edab32361ad8e321b0ffd9dc08181e8fc5b70d0
incubator4 <aries0robin@gmail.com>
2024-03-21 17:25:22 +0800

    fix(controller): fix svc wait ready

--- internal/controller/factory/k8s/svc.go
+++ internal/controller/factory/k8s/svc.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/util/wait"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"time"
 )
@@ -12,13 +13,13 @@ import (
 func HandleSvcUpdate(ctx context.Context, rc client.Client, svc *corev1.Service, waitDeadline time.Duration) error {
 	// Check if the service already exists, if not create a new one
 	var currentSvc corev1.Service
-	err := rc.Get(ctx, client.ObjectKey{Name: currentSvc.Name, Namespace: currentSvc.Namespace}, &currentSvc)
+	err := rc.Get(ctx, client.ObjectKey{Name: svc.Name, Namespace: svc.Namespace}, &currentSvc)
 	if err != nil {
 		if errors.IsNotFound(err) {
 			if err := rc.Create(ctx, svc); err != nil {
 				return fmt.Errorf("cannot create new service for app: %s, err: %w", svc.Name, err)
 			}
-			return nil
+			return waitServiceReady(ctx, rc, svc, waitDeadline)
 		}
 		return fmt.Errorf("cannot get service for app: %s err: %w", svc.Name, err)
 	}
@@ -27,5 +28,17 @@ func HandleSvcUpdate(ctx context.Context, rc client.Client, svc *corev1.Service,
 		return fmt.Errorf("cannot update service for app: %s, err: %w", svc.Name, err)
 	}
 
-	return nil
+	return waitServiceReady(ctx, rc, svc, waitDeadline)
+}
+
+func waitServiceReady(ctx context.Context, rc client.Client, svc *corev1.Service, waitDeadline time.Duration) error {
+	time.Sleep(2 * time.Second)
+	return wait.PollUntilContextTimeout(ctx, time.Second, waitDeadline, true, func(ctx context.Context) (bool, error) {
+		var actual corev1.Service
+		if err := rc.Get(ctx, client.ObjectKey{Name: svc.Name, Namespace: svc.Namespace}, &actual); err != nil {
+			return false, fmt.Errorf("cannot fetch actual statefulset: %w", err)
+		}
+
+		return false, nil
+	})
 }
